a4dd1f127352c411bd740e8fbb02bdf0dddbf2fe
juanrr950 <juanrr950@gmail.com>
2021-02-16 11:13:51 +0100

    problema ruta boton descarga

--- main/suc/carga_registro.py
+++ main/suc/carga_registro.py
@@ -54,64 +54,68 @@ def cargar_registros_txt(carga_masiva):
     print("Comenzamos abriendo el archivo, "+str(reg_inicio)+" nº de registro actuales")
     inicio=time.time()
     carga_masiva.inicio=datetime.now() 
-    
-    f = open(carga_masiva.csv.file.name, "r")
-    
-    lineas_totales=len(f.readlines())
-    f.seek(0)
-    carga_masiva.lineas_total=lineas_totales
-    carga_masiva.save()
-    
-    print("Archivo abierto en "+str((time.time()-inicio))+" s, total lineas:"+str(lineas_totales) )   
-    
-    
-    registros=[]
-    i=0
-    
-    for x in f:
-
-        i=i+1
-        linea=x.split(";")
-        poste_ori=linea[2]
-        poste=''.join(i for i in poste_ori if i.isdigit())
+    try:
+        f = open(carga_masiva.csv.file.name, "r")
         
-        if poste.isdigit():
-            poste=int(poste)
-            if poste>0 and poste< 4294967295 :
-                miga=linea[1]
-                if not miga.isdigit():
-                    miga=0
-                registros.append(Registro(id_poste=poste,
-                                          codigo_miga=miga,
-                                          codigo_suc=linea[0]))
-                if i%2000==0:
-                    devuelve=Registro.objects.bulk_create(registros,
-                                                  ignore_conflicts=True)
-                    registros=[]
-                    
-                    #print(len(devuelve))
-        '''    
-            else: 
-                print("Excluido poste: "+x)    
-        else:
-            print("Excluido poste: "+x)    
-        '''
-        if i%50000==0:
-            print("LLevamos "+str(i)+"  lineas, en "+
-                  str((time.time()-inicio)/60)+" min")
-            porc=round(i/lineas_totales*100)
-            carga_masiva.porcentaje_completado=porc
-            carga_masiva.save()
+        lineas_totales=len(f.readlines())
+        f.seek(0)
+        carga_masiva.lineas_total=lineas_totales
+        carga_masiva.save()
+        
+        print("Archivo abierto en "+str((time.time()-inicio))+" s, total lineas:"+str(lineas_totales) )   
+        
+        
+        registros=[]
+        i=0
+        
+        for x in f:
+    
+            i=i+1
+            linea=x.split(";")
+            poste_ori=linea[2]
+            poste=''.join(i for i in poste_ori if i.isdigit())
             
-    Registro.objects.bulk_create(registros,
-                            ignore_conflicts=True)
-    print("LLevamos "+str(i)+"  lineas, en "+
-                  str((time.time()-inicio)/60)+" min") 
-    
-    reg_fin=Registro.objects.count()
-    
-    print(str(reg_fin-reg_inicio)+" registros se han cargado nuevos")
-    
-    carga_masiva.registro_nuevos=reg_fin-reg_inicio
-    carga_masiva.estado='T'
+            if poste.isdigit():
+                poste=int(poste)
+                if poste>0 and poste< 4294967295 :
+                    miga=linea[1]
+                    if not miga.isdigit():
+                        miga=0
+                    registros.append(Registro(id_poste=poste,
+                                              codigo_miga=miga,
+                                              codigo_suc=linea[0]))
+                    if i%2000==0:
+                        devuelve=Registro.objects.bulk_create(registros,
+                                                      ignore_conflicts=True)
+                        registros=[]
+                        
+                        #print(len(devuelve))
+            '''    
+                else: 
+                    print("Excluido poste: "+x)    
+            else:
+                print("Excluido poste: "+x)    
+            '''
+            if i%50000==0:
+                print("LLevamos "+str(i)+"  lineas, en "+
+                      str((time.time()-inicio)/60)+" min")
+                porc=round(i/lineas_totales*100)
+                carga_masiva.porcentaje_completado=porc
+                carga_masiva.save()
+                
+        Registro.objects.bulk_create(registros,
+                                ignore_conflicts=True)
+        print("LLevamos "+str(i)+"  lineas, en "+
+                      str((time.time()-inicio)/60)+" min") 
+        
+        reg_fin=Registro.objects.count()
+        
+        print(str(reg_fin-reg_inicio)+" registros se han cargado nuevos")
+        
+        carga_masiva.registros_nuevos=reg_fin-reg_inicio
+        carga_masiva.estado='T'
+    except:
+        carga_masiva.estado='E'
+        
+    carga_masiva.inicio=datetime.now()     
     carga_masiva.save()
